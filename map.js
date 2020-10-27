b09bcd473d89f4d42c179c8e5626d65c0f5c70ec
TTibi1 <tibi30@freemail.hu>
2020-10-27 16:49:57 +0100

    bulletsMove fuggveny hozzaadasa: - a lovedekek felfele mozognak - a lovedekek ha elerik a palya legfelso savjat

--- map.js
+++ map.js
@@ -1,17 +1,23 @@
 const generateMap = (height, width) => {
     const arr = new Array(height);
-    for(let i = 0; i < height; i++){
+    for (let i = 0; i < height; i++) {
         arr[i] = new Array(width);
     };
     return arr;
 };
 
-
 const map = generateMap(30, 45);
 const bullets = []; // x, y
 const numbers = []; // x, y, num
-const player = [ {x: map.length-1, y: Math.floor(map[0].length/2), score: 0, life: 3}];
+const player = [{ x: map.length - 1, y: Math.floor(map[0].length / 2), score: 0, life: 3 }];
 
-
-
-//console.log(player);
+const bulletsMove = () => {
+    for (let i = 0; i < bullets.length; i++) {
+        if (bullets[i].x === 0) {
+            bullets.splice(i, 1);
+        };
+    };
+    for (let i = 0; i < bullets.length; i++) {
+        bullets[i].x--;
+    };
+};
