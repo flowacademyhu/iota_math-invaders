a206cf5311d893c29507d43fbda0bee8afdaab48
TTibi1 <tibi30@freemail.hu>
2020-11-02 09:28:22 +0100

    Javitott GUI terminal kittel

--- scoreboard.js
+++ scoreboard.js
@@ -1,25 +1,45 @@
 const readline = require('readline-sync');
+let terminalKit = require('terminal-kit').terminal;
 const { player } = require('./map');
 
 let playersDatabase = [
-    { name: 'Darth Vader', score: 23 },
-    { name: 'Han Solo', score: 18 },
+    { name: 'Darth Vader', score: 12 },
+    { name: 'Eukleidész', score: 18 },
+    { name: 'Erdős', score: 13 },
     { name: 'Stormtrooper', score: 0 }
 ];
 
 const getName = () => {
-    player.name = readline.question('Udv Jatekos! Kerjuk add meg a neved:');
+    player.name = readline.question('\x1b[93m\x1b[1mÜdv a játékban! Kérjük add meg a neved: \x1b[92m\x1b[1m ');
     playersDatabase.push(player);
 };
 
 const printScoreboard = () => {
-    let ranking = playersDatabase.sort((a, b) => {
+    let scoreboard = [['#', 'Játékos neve', 'Játékos pontszáma']];
+    
+    playersDatabase.sort((a, b) => {
         return b.score - a.score;
     });
 
-    for (let i = 0, k = 1; i < ranking.length; i++, k++) {      // rangsorolashoz kezdeti k valtozo
-        console.log(k + '.' + ' ' + ranking[i].name + ' ' + ranking[i].score);  // kesobb GUI-t kialakitani
+    for (let i = 0, k = 1; i < playersDatabase.length; i++, k++) {
+        scoreboard.push([k + '.', playersDatabase[i].name, playersDatabase[i].score]);
     };
+
+    terminalKit.table(scoreboard, {
+        hasBorder: true,
+        contentHasMarkup: true,
+        borderChars: 'heavy',
+        borderAttr: { color: 'white' },
+        textAttr: { bgColor: 'default' },
+        firstCellTextAttr: { bgColor: 'blue' },
+        firstRowTextAttr: { bgColor: 'red' },
+        firstColumnTextAttr: { bgColor: 'red' },
+        secondColumnTextAttr: { bgColor: 'red' },
+        width: 50,
+        lineWrap: false,
+        fit: true
+    }
+    );
 };
 
 module.exports = {
