bfa9b51df6772540917bde0a19131f9cadb0a683
TTibi1 <tibi30@freemail.hu>
2020-11-03 15:22:42 +0100

    scoreboard v2

--- scoreboard.js
+++ scoreboard.js
@@ -4,15 +4,32 @@ const { player, exercises } = require('./map');
 const rankJson = require('./ranking.json');
 const fs = require('fs');
 
+const pushName = () => {
+    for (let i = 0; i < rankJson.length; i++) {         // a json tartalmazza-e mar a nevet
+        if (rankJson[i].name === player.name) return true;
+    };
+};
+
 const getName = () => {
     player.name = readline.question('\x1b[93m\x1b[1mÜdv a játékban! Kérjük add meg a neved: \x1b[92m\x1b[1m ');
-    rankJson.push(player);
-    fs.writeFile('./ranking.json', JSON.stringify(rankJson), (err) => {
-    });
+    if (pushName()) {           // ha mar igen semmi
+    } else {                    // ha meg nem push
+        rankJson.push(player);
+        fs.writeFileSync('./ranking.json', JSON.stringify(rankJson), null, 2, (err) => {
+        });
+    };
 };
 
 const printScoreboard = () => {
+    for (let i = 0; i < rankJson.length; i++) {
+        if (rankJson[i].name === player.name) {
+            rankJson[i].score = player.score;
+            break;
+        };
+    };
+
     let scoreboard = [['#', 'Játékos neve', 'Játékos pontszáma']];
+
     rankJson.sort((a, b) => {
         return b.score - a.score;
     });
