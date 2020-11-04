39eb8b2be3a542bc6aab3fc48282e832442fa1b0
fhrmt <feher.mate.944@gmail.com>
2020-11-04 09:44:00 +0100

    merging develop into my branch

--- testing.js
+++ testing.js
@@ -1,5 +1,6 @@
 const figlet = require('figlet');
 const lolcatjs = require('lolcatjs');
+const chalk = require("chalk");
 
 // lolcatjs.options.seed = Math.round(Math.random() * 2);
 // lolcatjs.options.colors = true;
@@ -21,10 +22,26 @@ const lolcatjs = require('lolcatjs');
 
 //\nPress any key to continue!
 
-lolcatjs.fromString(figlet.textSync('Shoot all numbers in descending order!', {
+// lolcatjs.fromString(figlet.textSync('Shoot all\n\n\n numbers in descending order!', {
+//     font: 'ANSI Shadow',
+//     horizontalLayout: 'full',
+//     verticalLayout: 'full',
+//     width: 200,
+//     whitespaceBreak: true
+// }));
+
+console.log(chalk.bold.greenBright(figlet.textSync('You win!', {
     font: 'ANSI Shadow',
     horizontalLayout: 'full',
     verticalLayout: 'full',
     width: 200,
     whitespaceBreak: true
-}));
+})));
+
+console.log(chalk.bold.red(figlet.textSync('game over', {
+    font: 'ANSI Shadow',
+    horizontalLayout: 'full',
+    verticalLayout: 'full',
+    width: 200,
+    whitespaceBreak: true
+})));
